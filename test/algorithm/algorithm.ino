001b9c0fc2986eef0df83a9b71eafe5fd8413607
CaseyNelson <caseynelson314@gmail.com>
2023-04-15 16:41:09 +0900

    adsad

--- test/algorithm/algorithm.ino
+++ test/algorithm/algorithm.ino
@@ -1,5 +1,7 @@
-#include <FlexCAN_T4.h>
 
+#if defined(UDON_HAS_ARDUINO) && (defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__))
+#include <FlexCAN_T4.h>
+#endif
 void setup()
 {
 
