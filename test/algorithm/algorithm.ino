6975a85d3ba9c2086a3f3565d46c2d0c48231eac
CaseyNelson <caseynelson314@gmail.com>
2023-04-15 16:57:30 +0900

    push

--- test/algorithm/algorithm.ino
+++ test/algorithm/algorithm.ino
@@ -1,7 +1,8 @@
 
-#if defined(UDON_HAS_ARDUINO) && (defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__))
+#if defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__)
 #include <FlexCAN_T4.h>
 #endif
+
 void setup()
 {
 
