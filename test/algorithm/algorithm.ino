5822b74c4414525477fbc3e1f594be82141a7a9e
CaseyNelson <caseynelson314@gmail.com>
2023-04-23 15:58:51 +0900

    adsad

--- test/algorithm/algorithm.ino
+++ test/algorithm/algorithm.ino
@@ -1,14 +1,14 @@
 
-#if defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__)
-#include <FlexCAN_T4.h>
-#endif
+// #if defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) || defined(__IMXRT1062__)
+// #include <FlexCAN_T4.h>
+// #endif
 
-#include <FastLED.h>
+// #include <FastLED.h>
 
-#include <Wire.h>
-#include <SPI.h>
+// #include <Wire.h>
+// #include <SPI.h>
 
-// #include <test.hpp>
+#include <test.hpp>
 
 void setup()
 {
